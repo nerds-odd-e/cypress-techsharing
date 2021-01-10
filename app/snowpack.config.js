1e2ed4869c51e4900bd8d8750d5d786c35a62c0b
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-01-11 00:21:10 +0800

    declare REST-API proxy and setup store and api abstraction

--- app/snowpack.config.js
+++ app/snowpack.config.js
@@ -1,10 +1,13 @@
 // Consult https://www.snowpack.dev to learn about these options
 module.exports = {
-	extends: '@sveltejs/snowpack-config',
-	mount: {
-		'src/components': '/_components'
-	},
-	alias: {
-		$components: './src/components'
-	}
+  extends: "@sveltejs/snowpack-config",
+  mount: {
+    "src/components": "/_components",
+  },
+  alias: {
+    $components: "./src/components",
+  },
+  proxy: {
+    "/api": "http://localhost:8080/api",
+  },
 };
