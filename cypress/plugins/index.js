493fcc87f532bc18794fa56b5aa1042a9270ba72
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-06-13 17:56:16 +0800

    upgrade @percy/cypress to 3.1

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,6 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-let percyHealthCheck = require("@percy/cypress/task");
 
 /**
  * @type {Cypress.PluginConfig}
@@ -19,5 +18,4 @@ let percyHealthCheck = require("@percy/cypress/task");
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  on("task", percyHealthCheck);
 };
