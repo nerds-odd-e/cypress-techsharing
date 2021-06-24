97260b246bb9f6c4e1fa6a929e7db1c2c30084f1
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-06-24 11:41:53 +0800

    more dummy data from springboot API app

--- src/main/java/com/odde/demo/techsharing/Initializer.java
+++ src/main/java/com/odde/demo/techsharing/Initializer.java
@@ -25,14 +25,23 @@ class Initializer implements CommandLineRunner {
             "Shenzhen Tech Community")
         .forEach(name -> repository.save(new Group(name)));
 
-    Group techshare = repository.findByName("Shenzhen Tech Community");
-    Event e = Event.builder()
-                  .title("Full Stack Reactive")
-                  .description("Reactive with Spring Boot + Svelte")
-                  .date(Instant.parse("2020-12-12T15:00:00.000Z"))
-                  .build();
-    techshare.setEvents(Collections.singleton(e));
-    repository.save(techshare);
+    Group techshare1 = repository.findByName("Dongguan Tech Community");
+    Event e1 = Event.builder()
+                   .title("Full Stack Reactive")
+                   .description("Reactive with Spring Boot + Svelte")
+                   .date(Instant.parse("2021-07-12T15:00:00.000Z"))
+                   .build();
+    techshare1.setEvents(Collections.singleton(e1));
+    repository.save(techshare1);
+
+    Group techshare2 = repository.findByName("Shenzhen Tech Community");
+    Event e2 = Event.builder()
+                   .title("Visual Testing with Percy")
+                   .description("Visual Testing with Cypress + Percy")
+                   .date(Instant.parse("2021-09-15T17:30:00.000Z"))
+                   .build();
+    techshare2.setEvents(Collections.singleton(e2));
+    repository.save(techshare2);
 
     repository.findAll().forEach(System.out::println);
   }
