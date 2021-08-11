98dcda1151783e252194f123e7d9a4724df87f45
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-08-11 15:45:55 +0800

    solution to homework

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -4,14 +4,8 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
-describe("Home Page", function() {
-  it("Loads with expected Home, Groups and Visit cypress.io buttons", function() {
-    cy.visit("/");
-    cy.findByText(/Home/i);
-    cy.findByText(/Groups/i);
-    cy.findByText(/Visit cypress.io to read the documentation/i);
-    
-    cy.fixture('svg/cypress_io_testing_hourglass.svg').as('expected_logo');
-    cy.findByAltText('End-to-End Testing with Cypress.io Introductory Technical Workshop')
+describe('Home Page', function () {
+  it('Loads with expected Home, Groups and Visit cypress.io buttons', function () {
+    cy.verifyHomePage();
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,3 +24,47 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import '@testing-library/cypress/add-commands';
+
+Cypress.Commands.add('verifyHomePage', () => {
+  cy.visit('/');
+  cy.findByText(/Home/i);
+  cy.findByText(/Groups/i);
+  cy.findByText(/Visit cypress.io to read the documentation/i);
+
+  cy.findByAltText(
+    'End-to-End Testing with Cypress.io Introductory Technical Workshop'
+  );
+});
+
+Cypress.Commands.add('editAndVerifyLocation', (groupName) => {
+  cy.visit('/groups');
+  cy.findByRole('cell', { name: groupName })
+    .parent()
+    .findByRole('button', { name: 'Edit' })
+    .click()
+    .then(() => {
+      cy.url().should('include', '/group/3');
+      cy.findByRole('textbox', { name: /name/i });
+      cy.findByRole('textbox', { name: /address/i })
+        .clear()
+        .type('2012 Shennan Blvd., Futian District');
+      cy.findByRole('textbox', { name: /city/i }).clear().type('Shenzhen');
+      cy.findByRole('textbox', { name: /state\/province/i })
+        .clear()
+        .type('Guangdong');
+      cy.findByRole('textbox', { name: /country/i })
+        .clear()
+        .type("People's Republic of China");
+      cy.findByRole('textbox', { name: /postal code/i })
+        .clear()
+        .type('518038');
+      cy.findByRole('button', { name: 'Submit' })
+        .click()
+        .then(() => {
+          cy.contains('Shenzhen Tech Community');
+          cy.contains(
+            "2012 Shennan Blvd., Futian District Shenzhen Guangdong People's Republic of China 518038"
+          );
+        });
+    });
+});
