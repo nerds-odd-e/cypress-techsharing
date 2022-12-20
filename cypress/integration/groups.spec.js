ad4166f4d6f95b53f2f5f7d54d2abcfe1630a215
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-12-20 14:10:06 +0800

    upgrade cypress@12.1 & gradle@7.6

--- cypress/integration/groups.spec.js
+++
@@ -1,7 +0,0 @@
-// groups.spec.js created with Cypress
-//
-describe('Groups Page', function () {
-  it('Updates Shenzhen Tech Community Location after edit', function () {
-    cy.editAndVerifyLocation('Shenzhen Tech Community');
-  });
-});
